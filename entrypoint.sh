819eb9e6e5a3883669cc9f8a67bb5aeb1fae1794
Nebil <nebilfentaw864@gmail.com>
2026-09-13 23:16:29 -0700

    modified and fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py create_owner || true
 exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
